132018819bed55f7b2cce2ba18b4447ee8ae204b
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-11-07 12:00:38 +0200

    no changes added, state of apps to abck where they worked

--- ProInDev/ProInDev/content/forms.py
+++ ProInDev/ProInDev/content/forms.py
@@ -2,12 +2,16 @@
 from django import forms
 from ProInDev.content.models import Content, Comment
 
+
 class ContentForm(forms.ModelForm):
     class Meta:
         model = Content
         fields = ['title', 'body', 'visibility']
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'content']
+
+
